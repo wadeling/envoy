b1d1c74132b2c164fb45987c253e164fc251e56d
ericling <ericling@tencent.com>
2019-05-23 15:12:24 +0800

    add debug

--- source/common/http/codec_client.cc
+++ source/common/http/codec_client.cc
@@ -72,7 +72,9 @@ void CodecClient::onEvent(Network::ConnectionEvent event) {
   }
 
   int tmpSize = int(active_requests_.size());
-  ENVOY_CONN_LOG(trace, "type_ {},event {},active_requests_ size {}", *connection_,type_,event,tmpSize);
+  int tmpType = int(type_);
+  int tmpEvent = int(event);
+  ENVOY_CONN_LOG(trace, "type {} event {} active_requests size {}", *connection_,tmpType,tmpEvent,tmpSize);
 
   // HTTP/1 can signal end of response by disconnecting. We need to handle that case.
   if (type_ == Type::HTTP1 && event == Network::ConnectionEvent::RemoteClose &&
