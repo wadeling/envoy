ca3a509d60d121e29d5ab4b70becb19877e9d74b
ericling <ericling@tencent.com>
2019-06-05 18:27:26 +0800

    1

--- source/extensions/filters/http/grpc_http1_bridge/http1_bridge_filter.cc
+++ source/extensions/filters/http/grpc_http1_bridge/http1_bridge_filter.cc
@@ -32,7 +32,7 @@ Http::FilterHeadersStatus Http1BridgeFilter::decodeHeaders(Http::HeaderMap& head
   const absl::optional<Http::Protocol>& protocol = decoder_callbacks_->streamInfo().protocol();
   ASSERT(protocol);
 
-  ENVOY_LOG(trace,"protocol value {}",protocol.value());
+  ENVOY_LOG(trace,"protocol value {}",int(protocol.value()));
 
   if (protocol.value() != Http::Protocol::Http2 && grpc_request) {
     do_bridging_ = true;
