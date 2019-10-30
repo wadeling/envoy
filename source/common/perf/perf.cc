8b984aa67d2d0160a4f309c919527d4b87f2fdd8
wadeling <wadeling@gmail.com>
2019-10-30 21:23:28 +0800

    add time func

--- source/common/perf/perf.cc
+++ source/common/perf/perf.cc
@@ -1,15 +1,46 @@
-#include "perf.h"
+#include "common/perf/perf.h"
 
 namespace ENVOY {
+    typedef std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds> usType;
 
     std::vector<LatencyRecord> LatencyRecordArr(MAX_RECORD_NUM);
 
-    int ENVOY::BufferToSmallCount = 0;
+    int BufferToSmallCount = 0;
+
+    uint64_t get_current_time() {
+        usType tp = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now());
+        return tp.time_since_epoch().count();
+    }
 
     int record_server_rcv_time(int id) {
         if (id + 1 > MAX_RECORD_NUM) {
             return -1;
         }
-        LatencyRecordArr[id].server_rcv_time = ;
+        LatencyRecordArr[id].server_rcv_time = get_current_time();
+        return 0;
+    }
+
+    int record_server_send_time(int id) {
+        if (id + 1 > MAX_RECORD_NUM) {
+            return -1;
+        }
+        LatencyRecordArr[id].server_send_time = get_current_time();
+        return 0;
+    }
+
+    int record_client_send_time(int id) {
+        if (id + 1 > MAX_RECORD_NUM) {
+            return -1;
+        }
+        LatencyRecordArr[id].client_send_time = get_current_time();
+        return 0;
+    }
+
+    int record_client_rcv_time(int id) {
+        if (id + 1 > MAX_RECORD_NUM) {
+            return -1;
+        }
+        LatencyRecordArr[id].client_rcv_time = get_current_time();
+        return 0;
     }
 }
--- source/common/perf/perf.h
+++ source/common/perf/perf.h
@@ -3,17 +3,21 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <chrono>
 
 #define  MAX_RECORD_NUM     (60*4*60*1000)  // 60 concurrency,4 min,60sec,1000qps
 namespace ENVOY {
 
     struct LatencyRecord {
-        int server_rcv_time;
-        int server_send_time; // envoy rsp to client as server
-        int client_send_time; // envoy send to cluster as client
-        int client_rcv_time;
+        uint64_t server_rcv_time;
+        uint64_t server_send_time; // envoy rsp to client as server
+        uint64_t client_send_time; // envoy send to cluster as client
+        uint64_t client_rcv_time;
     };
 
     extern int BufferToSmallCount;
-    int record_server_rcv_time(int);
+    extern int record_server_rcv_time(int);
+    extern int record_server_send_time(int);
+    extern int record_client_rcv_time(int);
+    extern int record_client_send_time(int);
 }
